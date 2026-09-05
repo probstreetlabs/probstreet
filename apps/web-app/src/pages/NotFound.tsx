c3c647969fd65c8e090edfb9d35aba5012f53794
Rehan <official.rehan.me@gmail.com>
2026-09-05 21:59:49 +0530

    daily git push ✅

--- apps/web-app/src/pages/NotFound.tsx
+++ apps/web-app/src/pages/NotFound.tsx
@@ -4,7 +4,7 @@ import NotFoundIcon from '@/assets/images/404.avif';
 export default function NotFoundPage() {
 	return (
 		<div className="flex flex-col items-center justify-center min-h-screen text-center px-2 bg-[#f4f4f5]">
-			<img src={NotFoundIcon} alt="404 Not Found" className="w-[420px] max-w-full mb-6" />
+			<img src={NotFoundIcon} alt="404 Not Found" className="w-105 max-w-full mb-6" />
 			<h1 className="md:text-4xl text-2xl font-semibold mb-4">Uh oh!</h1>
 			<p className="mb-6 text-sm text-[#757575]">
 				Sorry, the page you were looking for was not found
